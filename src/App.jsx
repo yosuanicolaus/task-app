8884a7237fd9f2f3a04a40fb1a5ed9246ff502ca
Yosua Nicolaus <cryodawn257@gmail.com>
2022-04-14 15:16:11 +0700

    create app functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,54 @@
-function App() {
-  return <div>Hello World</div>;
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      task: { text: "" },
+      tasks: [],
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+      },
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: { text: "" },
+    });
+  };
+
+  foo = (e) => {
+    e.preventDefault();
+    console.log("clicked, foo...");
+  };
+
+  render() {
+    const { task, tasks } = this.state;
+
+    return (
+      <div>
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor="taskInput">Enter task: </label>
+          <input
+            type="text"
+            id="taskInput"
+            onChange={this.handleChange}
+            value={task.text}
+          />
+          <button type="submit">Add task</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
